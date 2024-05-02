ae55ee6ff7961b3a3e4d6fa881954bbc8ff57da9
Allyhshn <bit.alison@gmail.com>
2024-05-01 23:29:45 -0300

    Setup application

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://bit.Alison.github.io/ReactViteOnGithubPages",
   plugins: [react()],
 })
